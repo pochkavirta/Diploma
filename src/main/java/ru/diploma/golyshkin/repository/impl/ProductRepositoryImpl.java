26661a0a1e0c233da81b1df9fde537b7d81e9646
GolyshkinDA <Golyshkin.DA@nsd.ru>
2022-12-20 00:51:51 +0300

    add select

--- src/main/java/ru/diploma/golyshkin/repository/impl/ProductRepositoryImpl.java
+++ src/main/java/ru/diploma/golyshkin/repository/impl/ProductRepositoryImpl.java
@@ -15,7 +15,11 @@ public class ProductRepositoryImpl implements ProductRepository {
     private NamedParameterJdbcTemplate jdbcTemplate;
     private ProductRowMapper rowMapper;
 
-    private static final String FIND_ALL_PRODUCTS = "SELECT * FROM products";
+    private static final String FIND_ALL_PRODUCTS = "SELECT pd.products_id, u.nickname,"
+            + " pd.name, pd.description, pd.price, pd.ranking, pd.registered, pd.enabled, pc.category"
+            + " FROM products pd"
+            + " JOIN USERS u ON u.users_id=pd.user_id "
+            + " JOIN products_category pc ON pc.products_category_id=pd.products_category_id";
 
     @Override
     public List<Product> findAllProducts() {
