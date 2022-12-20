944f38835c1da0e3179706b7a563373191893d9c
GolyshkinDA <Golyshkin.DA@nsd.ru>
2022-12-20 13:55:51 +0300

    fix running

--- src/main/java/ru/diploma/golyshkin/web/RootController.java
+++ src/main/java/ru/diploma/golyshkin/web/RootController.java
@@ -5,15 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.diploma.golyshkin.service.ProductService;
 
-import java.util.Arrays;
-
 @Controller
 public class RootController {
     private ProductService productService;
 
     @GetMapping("/")
     public String root(Model model) {
-        model.addAttribute("products", productService.findAllProducts());
+        // model.addAttribute("products", productService.findAllProducts());
         return "index";
     }
 }
